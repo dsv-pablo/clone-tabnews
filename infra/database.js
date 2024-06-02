d803a3d78c495dc69cff59ef2c1ee9eeda3f1b6d
Pablo Vieira <63684077+dsv-pablo@users.noreply.github.com>
2024-06-02 16:00:31 +0000

    Added ssl field on postgres Client object

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   try {
